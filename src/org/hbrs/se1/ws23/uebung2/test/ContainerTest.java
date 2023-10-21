531d2317f97cce8308f24ed531a4d8b73fc19599
tobiasenger <mail.tobiasenger@gmail.com>
2023-10-21 04:00:30 +0200

    Uebung 2 Zwischenstand 2

--- src/org/hbrs/se1/ws23/uebung2/test/ContainerTest.java
+++
@@ -1,5 +0,0 @@
-package org.hbrs.se1.ws23.uebung2.test;
-
-public class ContainerTest {
-
-}
--- test/org/hbrs/se1/ws23/uebung2/test/ContainerTest.java
+++ test/org/hbrs/se1/ws23/uebung2/test/ContainerTest.java
@@ -15,23 +15,68 @@ public class ContainerTest {
     Member member_2;
     Member member_3;
 
+    Member member_4;
+
     @BeforeEach
     public void setUp() {
         container = new Container();
         member_1 = new ConcreteMember(3);
         member_2 = new ConcreteMember(8);
         member_3 = new ConcreteMember(11);
+        member_4 = new ConcreteMember(3);
     }
 
     @Test
-    public void testAddFirst() {
-        int size;
+    public void testAddEmpty() {
         assertEquals(0, container.size());
         try {
             container.addMember(member_1);
-        } catch (ContainerException e) {
-            size = -1;
-        }
+        } catch (ContainerException e) { }
         assertEquals(1, container.size());
+    }
+
+    @Test
+    public void testAddNotEmpty() {
+        assertEquals(0, container.size());
+        try {
+            container.addMember(member_1);
+            assertEquals(1, container.size());
+            container.addMember(member_2);
+        } catch (ContainerException e) { }
+        assertEquals(2, container.size());
+    }
+
+    @Test
+    public void testAddAlreadyExisting() {
+        assertEquals(0, container.size());
+        try {
+            container.addMember(member_1);
+        } catch (ContainerException e) { }
+        assertThrows(ContainerException.class, () -> container.addMember(member_4));
+    }
+
+    @Test
+    public void testDeleteEmptyNotExisting() {
+        container.deleteMember(2);
+        assertEquals(0, container.size());
+    }
+
+    @Test
+    public void testDeleteNonEmptyNotExisting() {
+        try {
+            container.addMember(member_1);
+        } catch(ContainerException e) { }
+        container.deleteMember(2);
+        assertEquals(1, container.size());
+    }
+
+    @Test
+    public void testDeleteNonEmptyExisting() {
+        try {
+            container.addMember(member_1);
+        } catch(ContainerException e) { }
+        assertEquals(1, container.size());
+        container.deleteMember(3);
+        assertEquals(0, container.size());
     }
 }
